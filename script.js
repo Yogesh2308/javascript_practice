aee0cdaaadb2f57b4b9da904fc284d14e378012d
Yogesh Kumar <ak766992@gmail.com>
2023-10-08 00:58:22 +0530

    new data added

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 const newDate = new Date()
 const todayDay = newDate.getDay()
-const dayName = ["Sunday", "Monday","Tuesday","wednesday","Thursday","Friday","Saturday"]
+const dayName = ["Sunday", "Monday", "Tuesday", "wednesday", "Thursday", "Friday", "Saturday"]
 // console.log("Today' is:",dayName[todayDay])
 // console.log(`Today's Date: ${newDate}`)
 let hours = newDate.getHours()
@@ -23,8 +23,8 @@ hours = hours % 12 || 12
 // Expected Output : 'abeemrstw'
 
 let strings = "webmaster"
-function sortOrder (strings){
-    console.log(strings.split("").sort().join(""))
+function sortOrder(strings) {
+    // console.log(strings.split("").sort().join(""))
 }
 sortOrder(strings)
 
@@ -34,13 +34,77 @@ sortOrder(strings)
 
 const newString = 'the quick brown fox'
 
-function upperCase(newString){
+function upperCase(newString) {
     var arr1 = newString.split(" ")
     var newarr1 = []
-    for(var x = 0; x < arr1.length; x++){
-        newarr1.push(arr1[x].charAt(0).toUpperCase()+arr1[x].slice(1))
+    for (var x = 0; x < arr1.length; x++) {
+        newarr1.push(arr1[x].charAt(0).toUpperCase() + arr1[x].slice(1))
     }
-    console.log(newarr1)
+    // console.log(newarr1)
     return newarr1.join(' ')
 }
 upperCase(newString)
+
+
+// Write a JavaScript function that takes an array of numbers and finds the second lowest and second greatest numbers, respectively.
+// Sample array : [1,2,3,4,5]
+// Expected Output : 2,4
+
+const ar = [1, 2, 3, 4, 5];
+
+function findSecondLowestAndGreatest(ar) {
+    const secondLowest = ar[1]
+    const secondGreatestNumber = ar[ar.length - 2]
+
+    return [secondLowest, secondGreatestNumber]
+}
+
+// console.log(findSecondLowestAndGreatest(ar))
+
+// Write a JavaScript function that accepts a list of country names as input and returns the longest country name as output.
+// Sample function : Longest_Country_Name(["Australia", "Germany", "United States of America"])
+// Expected output : "United States of America"
+
+const names = ["Australia", "Germany", "United States of America"]
+
+function longestName(names) {
+    let longestName = ""
+
+    for (const name of names) {
+        if (name.length > longestName.length) {
+            longestName = name
+        }
+    }
+    return longestName
+}
+// console.log(longestName(names))
+
+
+const beasts = "abacddbec";
+
+function repeatedCharacter(beasts) {
+    for (let i = 0; i < beasts.length; i++) {
+        const chr = beasts[i]
+        if(beasts.indexOf(chr) === i && beasts.lastIndexOf(chr) === i) {
+            return chr
+        }
+    }
+}
+
+console.log(repeatedCharacter(beasts))
+
+
+const header = document.querySelector('.sticky-header');
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener('scroll', () => {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos) {
+    // Scrolling up, show the header
+    header.style.top = '0';
+  } else {
+    // Scrolling down, hide the header
+    header.style.top = '-100px';
+  }
+  prevScrollPos = currentScrollPos;
+});
